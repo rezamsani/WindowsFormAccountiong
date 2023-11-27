1eff8ecb0f755f8735f81f95adb91c94ce7094c3
unknown <rezamsanitrrga@gmail.com>
2023-11-27 20:55:37 +0330

    Final_WindowsFormAccounting

---
+++ Accounting/Accounting.Business/Account.cs
@@ -0,0 +1,29 @@
+﻿using Accounting.DataLayer.Context;
+using Accounting.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Business
+{
+    public class Account
+    {
+        public static ReportViewModel ReportFormMain()
+        {
+            ReportViewModel rp = new ReportViewModel();
+            using(UnitOfWork db =new UnitOfWork())
+            {
+                DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 01);
+                DateTime endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 30);
+                var revcive = db.AccountingRepository.Get(a => a.TypeID == 1 && a.DateTitle >= startDate && a.DateTitle <= endDate).Select(a => a.Amount).ToList();
+                var pay = db.AccountingRepository.Get(a => a.TypeID == 2 && a.DateTitle >= startDate && a.DateTitle <= endDate).Select(a => a.Amount).ToList();
+                rp.Recive = revcive.Sum();
+                rp.Pay = pay.Sum();
+                rp.AccountBalance = (revcive.Sum() - pay.Sum());
+            }
+            return rp;
+        }
+    }
+}
